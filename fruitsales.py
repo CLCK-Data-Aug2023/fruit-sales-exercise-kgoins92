460cbef2b89f95914d170e13358204f2acaf7d9b
kgoins92 <kaitlyn.goins2010@gmail.com>
2023-12-29 13:53:25 -0500

    message

--- fruitsales.py
+++ fruitsales.py
@@ -1,1 +1,9 @@
-# add your code here
+import pandas as pd
+table = {'Apples' : pd.Series([35, 41],
+                              index=['2017 Sales', '2018 Sales']),
+         'Bananas' : pd.Series([21, 34],
+                               index=['2017 Sales', '2018 Sales'])}
+
+df = pd.DataFrame(table)
+
+print(df)
